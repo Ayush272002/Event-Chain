ae0db5d02fd9a04197f492d766bc6101eea3924b
Shay Patel <shay@shay.services>
2024-10-27 04:49:54 +0000

    🎫 Add event details on event page

--- app/events/[...eventId]/page.tsx
+++ app/events/[...eventId]/page.tsx
@@ -4,37 +4,19 @@ import { useParams } from 'next/navigation';
 import Header from '../../../components/custom/header';
 import Footer from '../../../components/custom/footer';
 import EventDescription from '../../../components/custom/EventDescription';
+import { fetchEventDetails } from '@/lib/fetchEventDetails';
 
 const ListingPage: React.FC = () => {
   const { eventId } = useParams();
   const [eventDetails, setEventDetails] = useState<any>(null);
 
   useEffect(() => {
-    const fetchEventDetails = async (id: number) => {
-      alert(`Fetching details for event ID: ${id}`);
-      // Dummy Response
-      const details = {
-        EventID: id,
-        name: 'Example Event Name',
-        date: '2023-12-01',
-        location: 'Example Location',
-        ticketPrice: 100,
-        description: 'Detailed description of the event.',
-        capacity: 300,
-        ticketsSold: 295,
-        imageUrl: [
-          'https://via.placeholder.com/150',
-          'https://via.placeholder.com/150',
-        ],
-        host: 'Example Host',
-        tickets: [1, 2, 3, 4],
-      };
-      return details;
-    };
-
     const getEventDetails = async () => {
       if (eventId) {
-        const details = await fetchEventDetails(Number(eventId));
+        const details = await fetchEventDetails({
+          eventID: Number(eventId),
+          toast: ({ title, variant }: any) => {alert(title);}});
+        console.log(details)
         setEventDetails(details);
       }
     };
