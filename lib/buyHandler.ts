85ad44cc648e77519cfb3f2cfcb9f8b992ecb162
Ayush Acharjya <ayushacharjya@gmail.com>
2024-10-27 01:03:00 +0100

    fix multiple tickers + abi

--- lib/buyHandler.ts
+++ lib/buyHandler.ts
@@ -33,42 +33,54 @@ export const buyHandler = async (
     return;
   }
 
-  try {
-    if (typeof window.ethereum === 'undefined') {
+  toast({
+    title:
+      'You might be asked to approve multiple transactions if you buy multiple tickets',
+  });
+
+  while (numTickets > 0) {
+    try {
+      if (typeof window.ethereum === 'undefined') {
+        toast({
+          title: 'Please install MetaMask or another Ethereum wallet',
+          variant: 'destructive',
+        });
+        return;
+      }
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = getContract().connect(signer);
+
+      const singleTicketCost = await contract.getEventPriceFlare(eventId);
+      const totalTicketCost = singleTicketCost
+        .mul(numTickets)
+        .mul(105)
+        .div(100);
+
+      const balance = await provider.getBalance(await signer.getAddress());
+      if (balance.lt(totalTicketCost)) {
+        toast({
+          title: 'Insufficient balance to cover ticket cost and gas fees.',
+          variant: 'destructive',
+        });
+        return;
+      }
+
+      const tx = await contract.buyTicket(eventId, { value: totalTicketCost });
+      const receipt = await tx.wait();
+
       toast({
-        title: 'Please install MetaMask or another Ethereum wallet',
+        title: `Tickets purchased successfully! Transaction Hash: ${receipt.transactionHash}`,
+      });
+
+      numTickets -= 1;
+    } catch (error) {
+      console.error('Error buying tickets:', error);
+      toast({
+        title: 'Transaction failed. Please try again.',
         variant: 'destructive',
       });
-      return;
     }
-
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const contract = getContract().connect(signer);
-
-    const singleTicketCost = await contract.getEventPriceFlare(eventId);
-    const totalTicketCost = singleTicketCost.mul(numTickets).mul(105).div(100);
-
-    const balance = await provider.getBalance(await signer.getAddress());
-    if (balance.lt(totalTicketCost)) {
-      toast({
-        title: 'Insufficient balance to cover ticket cost and gas fees.',
-        variant: 'destructive',
-      });
-      return;
-    }
-
-    const tx = await contract.buyTicket(eventId, { value: totalTicketCost });
-    const receipt = await tx.wait();
-
-    toast({
-      title: `Tickets purchased successfully! Transaction Hash: ${receipt.transactionHash}`,
-    });
-  } catch (error) {
-    console.error('Error buying tickets:', error);
-    toast({
-      title: 'Transaction failed. Please try again.',
-      variant: 'destructive',
-    });
   }
 };
